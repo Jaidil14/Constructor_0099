80194a23ecd99872a0e29e24d9fa060a361ce545
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-13 15:05:33 +0700

    Menambahkan class Mahasiswa serta member variabel dan functionnya

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa
+{
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+
+};
