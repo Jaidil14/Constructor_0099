bc3fc64e91b00ea7569e3a1e9208f008c7342626
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-13 15:18:56 +0700

    Menambahkan methode setID dan printAll

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -23,3 +23,16 @@ public:
 };
 
 int Mahasiswa::nim = 100;
+
+void Mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void Mahasiswa::printAll()
+{
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
