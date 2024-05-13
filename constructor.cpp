4d1468e847caf725084c43521e5a9a53f0068868
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-13 17:53:21 +0700

    Menambahkan fungsi utama

--- constructor.cpp
+++ constructor.cpp
@@ -19,8 +19,37 @@ public:
         nim = iNim;
     }
 
-    Mahasiswa(string);
+    Mahasiswa(string iNama);
     Mahasiswa(int iNim, string iNama);
-    void cetak();
+    void cetak()
+    {
+        cout << "NIM  = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+    }
 };
 
+Mahasiswa::Mahasiswa(string iNama)
+{
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+}
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(2);
+    Mahasiswa mhs3("Fikri");
+    Mahasiswa mhs4(4, "Ucel");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
