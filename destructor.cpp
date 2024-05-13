d6d335d8bdabad8abda6f80305005091183aeac1
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-13 17:38:36 +0700

    Menambahkan constructor, destructor, dan methode serta fungsi utama

--- destructor.cpp
+++ destructor.cpp
@@ -2,15 +2,56 @@
 #include <string>
 using namespace std;
 
-class angka
+class Angka
 {
 private:
     int *arr;
     int panjang;
 
 public:
-    angka(int); // Constructor
-    ~angka(); // Destructor
+    Angka(int); // Constructor
+    ~Angka(); // Destructor
     void cetakData();
     void isiData();
 };
+
+// Definisi member Function
+Angka::Angka(int i) // Constructor
+{
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+Angka::~Angka() //Destructor
+{
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat array sudah dilepaskan." << endl;
+}
+
+void Angka::cetakData()
+{
+    for(int i = 1; i < panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void Angka::isiData()
+{
+    for(int i = 1; i < panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    Angka belajarcpp(3); // Constructor dipanggil
+    Angka *ptrBelajarcpp = new angka(5); // Constructor dipanggil
+    delete ptrBelajarcpp; // Destructor dipanggil
+
+    return 0;
+}
