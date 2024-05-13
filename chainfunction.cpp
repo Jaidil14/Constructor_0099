61ed132d3bdde289bbd374944b0cafc0add2c1d7
Jaidil14 <jaidilrachmad@gmail.com>
2024-05-13 17:50:37 +0700

    Menambahkan class buku, member variabel dan function/methode-nya, kemudian menjadikan objek "bukunya". Lalu dipanggil dalam fungsi utama menggunakan chain function.

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class buku
+{
+public:
+    string judul;
+
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // Chain function
+    }
+    
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // Cout << bukunya.getJudul();
+
+    cout << bukunya.setJudul("Matematika").getJudul(); // Chain function calls
+
+    return 0;
+}
